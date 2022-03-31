3e8f9f0d2e387e60ff43ebdec594bc58fc6dd49a
RokasAbra <rokasabramavicius@gmail.com>
2022-03-31 18:28:51 +0300

    Devintas

--- js-nd/devintas.js
+++ js-nd/devintas.js
@@ -5,15 +5,16 @@ Parašykite programą, kuri surastų m-ųjų metų olimpinių žaidynių numerį
  turi būti spausdinama „Metai neolimpiniai“. Pasitikrinkite. Kai m = 1904, turi būti spausdinama: 
  n = 3. Kai m = 2005, turi būti spausdinama: Metai neolimpiniai.
 
-*/
- let pirmoSIosOlimpines = 1896;
- let olimpiniaiMetai = pirmoSIosOlimpines+=4;
- console.log(olimpiniaiMetai);
+*/ 
+m = 1912;
 
- console.log('-----------------');
+console.log(m)
 
- for (let i = 1896; i < 2022; i+=4) {
-     olimpiniaiMetai = i;
-     
-     console.log(i);
- }
+if ( (m - 1896 ) % 4 === 0 ) {
+    n = 1 + ((m - 1896) / 4) ;
+    console.log(`${m} metais vyko ${n}-osios olimpines zaidynes `);
+    
+} else { 
+    console.log('Pasirinkti metai neolimpiniai');
+    
+}
